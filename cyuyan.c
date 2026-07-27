075018f60531ab3853b570f051c8f045d6dec34d
chenlu-1103 <13326817323@163.com>
2026-07-27 15:01:34 +0800

    一个找零钱的c语言

---
+++ cyuyan.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int price = 0;
+	
+	printf("ÇëÊäÈë½ğ¶î: ");
+	scanf("%d", &price);
+	
+	int change = 100 - price;
+	
+	printf("ÕÒÄã%dÔª", change);
+	
+	return 0;
+}
